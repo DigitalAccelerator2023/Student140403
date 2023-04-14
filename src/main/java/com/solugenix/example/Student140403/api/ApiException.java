7c9c3c9c72d6c09eedc05f3a62ff0e46417d5ef1
MadhuKaranam <MadhuKaranam@BDSE-FB1ZN93>
2023-04-14 20:29:41 +0530

    Initial commit

---
+++ src/main/java/com/solugenix/example/Student140403/api/ApiException.java
@@ -0,0 +1,11 @@
+package com.solugenix.example.Student140403.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2023-04-14T20:29:29.101+05:30")
+
+public class ApiException extends Exception{
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
